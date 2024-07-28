292c3c3847928459995bc1d5ac226d73864eacc6
Samyam <samyambudhathokisc@gmail.com>
2024-07-28 16:05:57 +0545

    sasaasa

--- api/page.js
+++ api/page.js
@@ -1,16 +1,14 @@
-
-
-const express=require('express');
-const {ChatGoogleGenerativeAI}=require('@langchain/google-genai');
-const {HumanMessage}=require('@langchain/core/messages')
-const router=express.Router();
+const express = require("express");
+const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
+const { HumanMessage } = require("@langchain/core/messages");
+const router = express.Router();
 router.get("/", async (req, res) => {
   // const message = req.query.message;
-  const message="when did steph break ray allens record?"
+  const message = "when did steph break ray allens record?";
   if (typeof message === "string" && message) {
     const chat = new ChatGoogleGenerativeAI({
-      apiKey:`process.env.GOOGLE_API_KEY`,
-      model: 'gemini-pro',
+      apiKey: `process.env.GOOGLE_API_KEY`,
+      model: "gemini-pro",
       maxOutputTokens: 2048,
       openAIApiKey: process.env.GOOGLE_API_KEY,
       streaming: true,
@@ -23,14 +21,12 @@ router.get("/", async (req, res) => {
         },
       ],
     });
-    // We need to await the call to ensure that the
-    // connection is closed after the whole response
-    // is sent.
-    const chat1=await chat.call([new HumanMessage(message)]);
+
+    const chat1 = await chat.call([new HumanMessage(message)]);
     res.send(chat1);
     res.end();
   } else {
     res.json({ error: "No message provided" });
   }
 });
-module.exports=router;
+module.exports = router;
